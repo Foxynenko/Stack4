e12a2cf1a7783ee72fcd670b4da5294331d93cb8
vadim <vadim@DESKTOP-TQSCKM7>
2023-12-17 12:11:16 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class MyQueue {
+private:
+    stack<int> inputStack;
+    stack<int> outputStack;
+
+public:
+    // Конструктор класу
+    MyQueue() {}
+
+    // Переміщення елемента у кінець черги
+    void push(int x) {
+        inputStack.push(x);
+    }
+
+    // Вилучення елемента з початку черги та його повернення
+    int pop() {
+        if (outputStack.empty()) {
+            while (!inputStack.empty()) {
+                outputStack.push(inputStack.top());
+                inputStack.pop();
+            }
+        }
+
+        int frontElement = outputStack.top();
+        outputStack.pop();
+        return frontElement;
+    }
+
+    // Отримання елемента на початку черги
+    int peek() {
+        if (outputStack.empty()) {
+            while (!inputStack.empty()) {
+                outputStack.push(inputStack.top());
+                inputStack.pop();
+            }
+        }
+
+        return outputStack.top();
+    }
+
+    // Перевірка, чи черга порожня
+    bool empty() {
+        return inputStack.empty() && outputStack.empty();
+    }
+};
+
+int main() {
+    // Створення об'єкта MyQueue
+    MyQueue myQueue;
+
+    // Виклики методів для вказаних операцій
+    myQueue.push(1);
+    myQueue.push(2);
+    cout << "peek: " << myQueue.peek() << endl;
+    cout << "pop: " << myQueue.pop() << endl;
+    cout << "empty: " << (myQueue.empty() ? "true" : "false") << endl;
+
+    return 0;
+}
